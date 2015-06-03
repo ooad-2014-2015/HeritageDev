50368ac80e464f988ded6b4b724b2c156e96a5f0
ahavic1 <ahavic1>
2015-06-03 23:18:36 +0200

    LoginView i LoginVM update

--- DesktopAplikacija/it_shop/ViewModel/LoginViewModel.cs
+++ DesktopAplikacija/it_shop/ViewModel/LoginViewModel.cs
@@ -10,8 +10,10 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
-namespace it_shop.ViewModel {
-    public class LoginViewModel : INotifyPropertyChanged {
+namespace it_shop.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
         private LoginView loginForma;
         private ICommand button;
         private string username;
@@ -20,67 +22,75 @@ namespace it_shop.ViewModel {
 
         #region Properties
         public string Username
-	    {
-		    get { return username;}
-		    set 
-            { 
+        {
+            get { return username; }
+            set
+            {
                 username = value;
                 OnPropertyChanged("Username");
             }
-	    }
-	    public string Password
-	    {
-		    get { return password;}
-		    set
-            { 
+        }
+        public string Password
+        {
+            get { return password; }
+            set
+            {
                 password = value;
                 OnPropertyChanged("Password");
             }
-	    }
+        }
 
-        public ICommand Button {
+        public ICommand Button
+        {
             get { return button; }
             set { button = value; }
         }
 
         #endregion
 
-        public LoginViewModel (LoginView loginForma) {
+        public LoginViewModel(LoginView loginForma)
+        {
             Button = new RelayCommand(new Action(ValidirajLogin));
             this.loginForma = loginForma;
         }
 
-        public void KreirajNit ( ) {
-            Thread nit = new Thread(( ) => ValidirajLogin()) { IsBackground = true };
+        public void KreirajNit()
+        {
+            Thread nit = new Thread(() => ValidirajLogin()) { IsBackground = true };
             nit.Join();
         }
 
-        private MySqlDataReader UpitNaBazu ( string upit, MySqlConnection con ) {
+        private MySqlDataReader UpitNaBazu(string upit, MySqlConnection con)
+        {
             con.Open();
             MySqlCommand u = new MySqlCommand(upit, con);
             return u.ExecuteReader();
         }
-        private void ValidirajLogin ( ) {
+        private void ValidirajLogin()
+        {
             MySqlConnection con = new MySqlConnection("server=192.168.1.11; user=root; pwd=root; database=it_shop");
-            try {
+            try
+            {
                 string upit = "SELECT * FROM uposlenici WHERE username = '" + Username + "' AND password = '" + Password + "';";
 
-                Task<MySqlDataReader> nit = Task<MySqlDataReader>.Factory.StartNew(( ) => UpitNaBazu(upit, con));
+                Task<MySqlDataReader> nit = Task<MySqlDataReader>.Factory.StartNew(() => UpitNaBazu(upit, con));
 
                 MySqlDataReader r = nit.Result;
-                if (r.HasRows && r.Read()) {
+                if (r.HasRows && r.Read())
+                {
                     string tipuposlenika = r.GetString("tip_uposlenika");
-                    switch (tipuposlenika) {
+                    switch (tipuposlenika)
+                    {
                         case "PRODAVAC":
                             ProdavacView prodavac = new ProdavacView();
                             prodavac.Show();
                             loginForma.Close();
-                        break;
+                            break;
                         case "DIREKTOR":
                             DirectorView direktor = new DirectorView();
                             direktor.Show();
                             loginForma.Close();
-                        break;
+                            break;
                         case "SERVISER":
                         case "MONTER":
                             MessageBox.Show("Tip uposlenika: " + tipuposlenika);
@@ -89,30 +99,38 @@ namespace it_shop.ViewModel {
                             MessageBox.Show("Nema uposlenog sa tim tipom.");
                             break;
                     }
-                } else {
+                }
+                else
+                {
                     MessageBox.Show("Pogresni podaci.");
                     Username = String.Empty;
                     Password = String.Empty;
                 }
 
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 MessageBox.Show(ex.ToString());
             }
             con.Close();
         }
-    
 
-        
+
+
 
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged ( string propertyName ) {
-            if (PropertyChanged != null) {
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
-        protected void NotifyPropertyChanged ( string propertyName ) {
-            if (PropertyChanged != null) {
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
