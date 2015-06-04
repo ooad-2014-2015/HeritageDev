fc1d028089aa2c50517c2a2250e5e29eb88e3d9d
ssuljkanovic <salem.suljkanovic@gmail.com>
2015-06-04 17:43:10 +0200

    ProdavacVM

--- DesktopAplikacija/it_shop/ViewModel/ProdavacViewModel.cs
+++ DesktopAplikacija/it_shop/ViewModel/ProdavacViewModel.cs
@@ -34,8 +34,9 @@ namespace it_shop.ViewModel {
         private ICommand ponistiButton;
         private ICommand izaberiSlikuButton;
         private BitmapImage ucitajSliku;
-
         private string putanja;
+
+        #region Properties
         public BitmapImage UcitajSlikuBinding {
             get {
                 return ucitajSliku; 
@@ -45,8 +46,6 @@ namespace it_shop.ViewModel {
                 OnPropertyChanged("UcitajSlikuBinding");
             }
         }
-       
-        #region Properties
         public ICommand IzaberiSlikuButton {
             get { return izaberiSlikuButton; }
             set { izaberiSlikuButton = value; }
@@ -124,79 +123,7 @@ namespace it_shop.ViewModel {
         }
         #endregion Properties
 
-        public ProdavacViewModel ( ) {
-            UnesiButton = new RelayCommand(new Action(UnosArtikla));
-            PonistiButton = new RelayCommand(new Action(PonistiIzmjene));
-            IzaberiSlikuButton = new RelayCommand(new Action(IzaberiSliku));
-        }
-
-        private void IzaberiSliku ( ) {
-            Microsoft.Win32.OpenFileDialog openFileDialog1 = new Microsoft.Win32.OpenFileDialog();
-            openFileDialog1.Filter = "JPEG Files(.png)|*.jpg|All Files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 1;
-            openFileDialog1.Multiselect = false;
-            openFileDialog1.ShowDialog();
-            putanja = openFileDialog1.FileName;
-            if (putanja == string.Empty) {
-                MessageBox.Show("Niste odabrali datoteku.");
-            } else {
-                BitmapImage b = new BitmapImage();
-                b.BeginInit();
-                b.UriSource = new Uri(putanja, UriKind.RelativeOrAbsolute);
-                b.EndInit();
-                UcitajSlikuBinding = b;
-            }
-        }
-
-        private void UnosArtikla ( ) {
-            MySqlConnection con = new MySqlConnection("server=192.168.1.11; user=root; pwd=root; database=it_shop");
-
-            try {
-                string _mjeseciGarancije = MjeseciGarancije.Substring(37);
-                string _kategorijaProizvoda = KategorijaProizvoda.Substring(37);
-                string upit = "INSERT INTO artikli VALUES (" + IdProizvoda + ", '" + NazivProizvoda + "', '" + _kategorijaProizvoda + "', " + Cijena + ", '" + Opis + "', " + _mjeseciGarancije + ", '" + Proizvodjac + "', '" + DodatnaOprema + "', " + Kolicina + ", null)";
-
-                //Task<MySqlDataReader> nit = Task<MySqlDataReader>.Factory.StartNew(( ) => UpitNaBazu(upit, con));
-                con.Open();
-                MySqlCommand cmd = new MySqlCommand(upit, con);
-                cmd.ExecuteNonQuery();
-
-
-            } catch (Exception ex) {
-                MessageBox.Show(ex.ToString());
-            //} catch (System.AggregateException) {
-            //    MessageBox.Show("Neuspjela konekcija sa bazom podataka!\nPokušajte ponovo.");
-            //} catch (Exception ex) {
-            //    MessageBox.Show("Došlo je do greške!\nMolimo pokušajte ponovo ili kontaktirajte administratora!");
-            }
-            con.Close();
-
-        }
-
-        //public void KreirajNit ( ) {
-        //    Thread nit = new Thread(( ) => ValidirajLogin()) { IsBackground = true };
-        //    nit.Join();
-        //}
-
-        private MySqlDataReader UpitNaBazu ( string upit, MySqlConnection con ) {
-            con.Open();
-            MySqlCommand u = new MySqlCommand(upit, con);
-            return u.ExecuteReader();
-        }
-
-
-        private void PonistiIzmjene ( ) {
-            IdProizvoda = string.Empty;
-            NazivProizvoda = string.Empty;
-            KategorijaProizvoda = string.Empty;
-            Cijena = string.Empty;
-            Opis = string.Empty;
-            MjeseciGarancije = string.Empty;
-            Proizvodjac = string.Empty;
-            DodatnaOprema = string.Empty;
-            Kolicina = string.Empty;
-        }
-
+        #region INotify Implementation
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged ( string propertyName ) {
             if (PropertyChanged != null) {
@@ -208,7 +135,129 @@ namespace it_shop.ViewModel {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
+        #endregion
 
-        public ICommand ucitajsliku { get; set; }
+        public ProdavacViewModel ( ) {
+            UnesiButton = new RelayCommand(new Action(UnosNovogArtikla));
+            PonistiButton = new RelayCommand(new Action(PonistiIzmjene));
+            IzaberiSlikuButton = new RelayCommand(new Action(IzaberiSliku));
+            UcitajSlikuBinding = UcitajSliku(@"/it_shop;component/Resources/no_image.png");
+        }
+        private BitmapImage UcitajSliku( string _putanja ) {
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+            //b.UriSource = new Uri(System.IO.Path.GetFullPath(@"../../Resources/no_image.png"), UriKind.RelativeOrAbsolute);
+            b.UriSource = new Uri(_putanja, UriKind.RelativeOrAbsolute);
+            b.EndInit();
+            return b;
+        }
+        private void UpisiSliku ( ) {
+            MySqlConnection conn;
+            MySqlCommand cmd;
+
+            conn = new MySqlConnection();
+            cmd = new MySqlCommand();
+
+            string SQL;
+            int FileSize;
+            byte[] rawData;
+            FileStream fs;
+
+            conn.ConnectionString = "server=192.168.1.11;uid=root;pwd=root;database=it_shop;";
+
+            try {
+                fs = new FileStream(putanja, FileMode.Open, FileAccess.Read);
+                FileSize = Convert.ToInt32(fs.Length);
+
+                rawData = new byte[FileSize];
+                fs.Read(rawData, 0, FileSize);
+                fs.Close();
+
+                conn.Open();
+
+                SQL = "INSERT INTO slika VALUES(@FileName, @FileSize, @File)";
+
+                cmd.Connection = conn;
+                cmd.CommandText = SQL;
+                cmd.Parameters.AddWithValue("@FileName", "test");
+                cmd.Parameters.AddWithValue("@FileSize", FileSize);
+                cmd.Parameters.AddWithValue("@File", rawData);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("File Inserted into database successfully!",
+                    "Success!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                conn.Close();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex) {
+                MessageBox.Show("Error " + ex.Number + " has occurred: " + ex.Message,
+                    "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void IzaberiSliku ( ) {
+            Microsoft.Win32.OpenFileDialog openFileDialog1 = new Microsoft.Win32.OpenFileDialog();
+            openFileDialog1.Filter = "JPEG Files(.png)|*.jpg|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.ShowDialog();
+            putanja = openFileDialog1.FileName;
+            if (putanja == string.Empty) {
+                MessageBox.Show("Niste odabrali datoteku.");
+            } else {
+                UcitajSlikuBinding = UcitajSliku(putanja);
+            }
+        }
+        private void UnosNovogArtikla ( ) {
+            MySqlConnection con = new MySqlConnection("server=192.168.1.11; user=root; pwd=root; database=it_shop");
+            MySqlCommand cmd = new MySqlCommand();
+            int FileSize;
+            byte[] rawData;
+            FileStream fs;
+
+            try {
+                string _mjeseciGarancije = MjeseciGarancije.Substring(37);
+                string _kategorijaProizvoda = KategorijaProizvoda.Substring(37);
+                string upit = "INSERT INTO artikli VALUES (" + IdProizvoda + ", '" + NazivProizvoda + "', '" + _kategorijaProizvoda + "', " + Cijena + ", '" + Opis + "', " + _mjeseciGarancije + ", '" + Proizvodjac + "', '" + DodatnaOprema + "', " + Kolicina + ", ";
+
+                fs = new FileStream(putanja, FileMode.Open, FileAccess.Read);
+                FileSize = Convert.ToInt32(fs.Length);
+                rawData = new byte[FileSize];
+                fs.Read(rawData, 0, FileSize);
+                fs.Close();
+                cmd.Parameters.AddWithValue("@FileSize", FileSize);
+                cmd.Parameters.AddWithValue("@File", rawData);
+
+                upit += "@File, @FileSize);";
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = upit;                
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Artikal uspjesno unesen.");
+                PonistiIzmjene();
+
+            } catch (System.AggregateException) {
+                MessageBox.Show("Neuspjela konekcija sa bazom podataka!\nPokušajte ponovo.");
+            } catch (Exception ex) {
+                MessageBox.Show("Došlo je do greške!\nMolimo pokušajte ponovo ili kontaktirajte administratora!");
+            }
+            con.Close();
+
+        }
+        private void PonistiIzmjene ( ) {
+            IdProizvoda = string.Empty;
+            NazivProizvoda = string.Empty;
+            KategorijaProizvoda = string.Empty;
+            Cijena = string.Empty;
+            Opis = string.Empty;
+            MjeseciGarancije = string.Empty;
+            Proizvodjac = string.Empty;
+            DodatnaOprema = string.Empty;
+            Kolicina = string.Empty;
+            UcitajSlikuBinding = UcitajSliku(@"/it_shop;component/Resources/no_image.png");
+        }
+
+
     }
 }
