a1e522c8fefe7668317703976738d599da1ece6c
ahavic1 <ahavic1>
2015-06-04 20:48:11 +0200

    Direktor V i VM update

--- DesktopAplikacija/it_shop/ViewModel/DirektorViewModel.cs
+++ DesktopAplikacija/it_shop/ViewModel/DirektorViewModel.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
@@ -15,19 +16,121 @@ namespace it_shop.ViewModel
 {
     public class DirektorViewModel : INotifyPropertyChanged
     {
+       
+        public DirektorViewModel()
+        {
+            UcitajZahtjeve = new RelayCommand(new Action(UcitajZahjeveZaNabavkomIzBaze));
+            ObrisiUposlenika = new RelayCommand(new Action(ObrisiUposlenikaIzBaze));
+            ObrisiUposlenika = new RelayCommand(new Action(UnosNovogKorisnikaUBazu));
+            AzuzirajInfoUposlenika = new RelayCommand(new Action(AzurirajInformacijeKorisnika));
+        }
+
+        private MySqlDataReader UpitNaBazu(string upit, MySqlConnection con)
+        {
+            con.Open();
+            MySqlCommand u = new MySqlCommand(upit, con);
+            return u.ExecuteReader();
+        }
+
+        private void DMLUpitiNaBazu(string upit, MySqlConnection con)
+        {
+            con.Open();
+            MySqlCommand u = new MySqlCommand(upit, con);
+            u.ExecuteNonQuery();
+        }
+
+        #region Zahtjevi Za Nabavke - Tab 1
+
+        #region Atributi
+      
+        private ZahtjevZaNabavkom odabranizahtjev;
+        private ObservableCollection<string> listaZahtjeva;
+        private ICommand obrisiZahtjev;
+        private ICommand odobriZahtjev;
+        private ICommand ucitajZahtjeve;
+
+        #endregion
+
+      
+        #region Properties
+       
+        public ICommand UcitajZahtjeve
+        {
+            get { return ucitajZahtjeve; }
+            set { ucitajZahtjeve = value; }
+        }
+
+        public ICommand OdobriZahtjev
+        {
+            get { return odobriZahtjev; }
+            set { odobriZahtjev = value; }
+        }
+
+        public ICommand ObrisiZahtjev
+        {
+            get { return obrisiZahtjev; }
+            set { obrisiZahtjev = value; }
+        }
+
+        #endregion
+
+        
+        #region Metode
+       
+        //Zahtjeve ucitava trenutno ucitava uposlenike
+        private void UcitajZahjeveZaNabavkomIzBaze()
+        {
+            MySqlConnection connectionBaza = new MySqlConnection("server=192.168.1.11; user=root; pwd=root; database=it_shop");
+
+            try
+            {
+
+                string upitBaza = "SELECT * FROM uposlenici;";
+                string naziv, spol, telefon, adresa, datumZaposlenja;
+                double plata, dodatak;
+                int godisnji;
+                // DateTime zaposlenjeDatum;
+
+                MySqlDataReader r = UpitNaBazu(upitBaza, connectionBaza);
+                while (r.Read())
+                {
+                    naziv = r.GetString("ime_i_prezime");
+                    spol = r.GetString("spol");
+                    adresa = r.GetString("adresa");
+                    telefon = r.GetString("broj_telefona");
+                    //datumZaposlenja = r.GetString("datum_zaposlenja");
+                    plata = double.Parse(r.GetString("plata"), System.Globalization.CultureInfo.InvariantCulture);
+                    dodatak = double.Parse(r.GetString("dodatak_na_platu"), System.Globalization.CultureInfo.InvariantCulture);
+                    // zaposlenjeDatum = DateTime.ParseExact("datumZapolenja", "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                    godisnji = Int32.Parse(r.GetString("dani_godisnjeg_odmora"));
+
+                    Uposlenik tmp = new Uposlenik(naziv, adresa, telefon, DateTime.Now, spol, plata, dodatak, godisnji);
+                    //listaUposlenika.Add(naziv + " " + spol + " " + adresa + " " + telefon);
+                    //ListaUposlenika.Add(tmp);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+        
+        #endregion
+       
+        #endregion
+
+        #region Pregled Zaposlenika - Tab 2
+
+        #region Atributi
+
         private int odabraniTab;
         private Uposlenik odabraniUposlenik;
         private List<Uposlenik> uposleniciArhiva = new List<Uposlenik>();
         private ObservableCollection<Uposlenik> listaUposlenika = new ObservableCollection<Uposlenik>();
-        private ZahtjevZaNabavkom odabranizahtjev;
-        private ObservableCollection<string> listaZahtjeva;
-
-        private ICommand obrisiZahtjev;
-        private ICommand odobriZahtjev;
-        private ICommand ucitajZahtjeve;
         private ICommand obrisiUposlenika;
         private ICommand azuzirajInfoUposlenika;
-
         private string imeAzuriraj;
         private string prezimeAzuriraj;
         private string spolAzuriraj;
@@ -36,89 +139,15 @@ namespace it_shop.ViewModel
         private string tipUposlenikaAzuriraj;
         private string datumZaposlenjaAzuriraj;
         private string plataAzuriraj;
-        private string dodatakNaPlatu;
+        private string dodatakNaPlatuAzuriraj;
         private string daniGodisnjegAzuriraj;
         private string usernameAzuriraj;
         private string passwordAzuriraj;
 
-        #region Properties Azuriranje Uposlenika
-
-        public string PasswordAzuriraj
-        {
-            get { return passwordAzuriraj; }
-            set { passwordAzuriraj = value; OnPropertyChanged("PasswordAzuriraj"); }
-        }
-        
-        public string UsernameAzuriraj
-        {
-            get { return usernameAzuriraj; }
-            set { usernameAzuriraj = value; OnPropertyChanged("UsernameAzuriraj"); }
-        }
-        
-        public string DaniGodisnjegAzuriraj
-        {
-            get { return daniGodisnjegAzuriraj; }
-            set { daniGodisnjegAzuriraj = value; OnPropertyChanged("DaniGodisnjegOdmora"); }
-        }
-        
-        public string DodatakNaPlata
-        {
-            get { return dodatakNaPlatu; }
-            set { dodatakNaPlatu = value; OnPropertyChanged("DodatakNaPlatu"); }
-        }
-        
-        public string PlataAzuriraj
-        {
-            get { return plataAzuriraj; }
-            set { plataAzuriraj = value; OnPropertyChanged("PlataAzuriraj"); }
-        }
-        
-        public string DatumZaposlenjaAzuriraj
-        {
-            get { return datumZaposlenjaAzuriraj; }
-            set { datumZaposlenjaAzuriraj = value; OnPropertyChanged("DatumZaposlenjaAzuriraj"); }
-        }
-        
-        public string TipUposlenikaAzuriraj
-        {
-            get { return tipUposlenikaAzuriraj; }
-            set { tipUposlenikaAzuriraj = value; OnPropertyChanged("TipUposlenikaAzuriraj"); }
-        }
-        
-        public string AdresaAzuriraj
-        {
-            get { return adresaAzuriraj; }
-            set { adresaAzuriraj = value; OnPropertyChanged("AdresaAzuriraj"); }
-        }
-        
-
-        public string BrojTelefonaAzuriraj
-        {
-            get { return brojTelefonaAzuriraj; }
-            set { brojTelefonaAzuriraj = value; OnPropertyChanged("BrojTelefonaAzuriraj"); }
-        }
-        
-        public string SpolAzuriraj
-        {
-            get { return spolAzuriraj; }
-            set { spolAzuriraj = value; OnPropertyChanged("SpolAzuriraj"); }
-        }
-        
-        public string PrezimeAzuriraj
-        {
-            get { return prezimeAzuriraj; }
-            set { prezimeAzuriraj = value; OnPropertyChanged("PrezimeAzuriraj"); }
-        }
-        
-        public string ImeAzuriraj
-        {
-            get { return imeAzuriraj; }
-            set { imeAzuriraj = value; OnPropertyChanged("ImeAzuriraj"); }
-        }
-
         #endregion
-        
-        #region Properties
+
+     
+        #region Properties Azuriranje Uposlenika
 
         public ICommand AzuzirajInfoUposlenika
         {
@@ -131,7 +160,7 @@ namespace it_shop.ViewModel
             get { return obrisiUposlenika; }
             set { obrisiUposlenika = value; }
         }
-
+       
         public int OdabraniTab
         {
             get { return odabraniTab; }
@@ -187,108 +216,84 @@ namespace it_shop.ViewModel
                 OnPropertyChanged("OdabraniUposlenik");
             }
         }
-
-        public ICommand UcitajZahtjeve
+       
+        public string PasswordAzuriraj
         {
-            get { return ucitajZahtjeve; }
-            set { ucitajZahtjeve = value; }
+            get { return passwordAzuriraj; }
+            set { passwordAzuriraj = value; OnPropertyChanged("PasswordAzuriraj"); }
         }
 
-        public ICommand OdobriZahtjev
+        public string UsernameAzuriraj
         {
-            get { return odobriZahtjev; }
-            set { odobriZahtjev = value; }
+            get { return usernameAzuriraj; }
+            set { usernameAzuriraj = value; OnPropertyChanged("UsernameAzuriraj"); }
         }
 
-        public ICommand ObrisiZahtjev
+        public string DaniGodisnjegAzuriraj
         {
-            get { return obrisiZahtjev; }
-            set { obrisiZahtjev = value; }
+            get { return daniGodisnjegAzuriraj; }
+            set { daniGodisnjegAzuriraj = value; OnPropertyChanged("DaniGodisnjegAzuriraj"); }
         }
 
+        public string DodatakNaPlatuAzuriraj
+        {
+            get { return dodatakNaPlatuAzuriraj; }
+            set { dodatakNaPlatuAzuriraj = value; OnPropertyChanged("DodatakNaPlatuAzuriraj"); }
+        }
+
+        public string PlataAzuriraj
+        {
+            get { return plataAzuriraj; }
+            set { plataAzuriraj = value; OnPropertyChanged("PlataAzuriraj"); }
+        }
+
+        public string DatumZaposlenjaAzuriraj
+        {
+            get { return datumZaposlenjaAzuriraj; }
+            set { datumZaposlenjaAzuriraj = value; OnPropertyChanged("DatumZaposlenjaAzuriraj"); }
+        }
+
+        public string TipUposlenikaAzuriraj
+        {
+            get { return tipUposlenikaAzuriraj; }
+            set { tipUposlenikaAzuriraj = value; OnPropertyChanged("TipUposlenikaAzuriraj"); }
+        }
+
+        public string AdresaAzuriraj
+        {
+            get { return adresaAzuriraj; }
+            set { adresaAzuriraj = value; OnPropertyChanged("AdresaAzuriraj"); }
+        }
+
+
+        public string BrojTelefonaAzuriraj
+        {
+            get { return brojTelefonaAzuriraj; }
+            set { brojTelefonaAzuriraj = value; OnPropertyChanged("BrojTelefonaAzuriraj"); }
+        }
+
+        public string SpolAzuriraj
+        {
+            get { return spolAzuriraj; }
+            set { spolAzuriraj = value; OnPropertyChanged("SpolAzuriraj"); }
+        }
+
+        public string PrezimeAzuriraj
+        {
+            get { return prezimeAzuriraj; }
+            set { prezimeAzuriraj = value; OnPropertyChanged("PrezimeAzuriraj"); }
+        }
+
+        public string ImeAzuriraj
+        {
+            get { return imeAzuriraj; }
+            set { imeAzuriraj = value; OnPropertyChanged("ImeAzuriraj"); }
+        }
 
         #endregion
 
-        #region INotify Implementation
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        protected void NotifyPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        #endregion
 
-
-        public DirektorViewModel()
-        {
-            UcitajZahtjeve = new RelayCommand(new Action(UcitajZahjeveZaNabavkomIzBaze));
-            ObrisiUposlenika = new RelayCommand(new Action(ObrisiUposlenikaIzBaze));
-            ObrisiUposlenika = new RelayCommand(new Action(UnosNovogKorisnikaUBazu));
-        }
-
-
-        private MySqlDataReader UpitNaBazu(string upit, MySqlConnection con)
-        {
-            con.Open();
-            MySqlCommand u = new MySqlCommand(upit, con);
-            return u.ExecuteReader();
-        }
-
-        private void DMLUpitiNaBazu(string upit, MySqlConnection con)
-        {
-            con.Open();
-            MySqlCommand u = new MySqlCommand(upit, con);
-            u.ExecuteNonQuery();
-        }
-
-        private void UcitajZahjeveZaNabavkomIzBaze()
-        {
-            MySqlConnection connectionBaza = new MySqlConnection("server=192.168.1.11; user=root; pwd=root; database=it_shop");
-
-            try
-            {
-
-                string upitBaza = "SELECT * FROM uposlenici;";
-                string naziv, spol, telefon, adresa, datumZaposlenja;
-                double plata, dodatak;
-                int godisnji;
-                // DateTime zaposlenjeDatum;
-
-                MySqlDataReader r = UpitNaBazu(upitBaza, connectionBaza);
-                while (r.Read())
-                {
-                    naziv = r.GetString("ime_i_prezime");
-                    spol = r.GetString("spol");
-                    adresa = r.GetString("adresa");
-                    telefon = r.GetString("broj_telefona");
-                    //datumZaposlenja = r.GetString("datum_zaposlenja");
-                    plata = double.Parse(r.GetString("plata"), System.Globalization.CultureInfo.InvariantCulture);
-                    dodatak = double.Parse(r.GetString("dodatak_na_platu"), System.Globalization.CultureInfo.InvariantCulture);
-                    // zaposlenjeDatum = DateTime.ParseExact("datumZapolenja", "dd/MM/yyyy", CultureInfo.InvariantCulture);
-                    godisnji = Int32.Parse(r.GetString("dani_godisnjeg_odmora"));
-
-                    Uposlenik tmp = new Uposlenik(naziv, adresa, telefon, DateTime.Now, spol, plata, dodatak, godisnji);
-                    //listaUposlenika.Add(naziv + " " + spol + " " + adresa + " " + telefon);
-                    //ListaUposlenika.Add(tmp);
-                }
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
-
-        }
-
+        #region Metode
         private void UcitajUposlenikeIzBaze()
         {
             if (ListaUposlenika.Count == 0)
@@ -316,7 +321,7 @@ namespace it_shop.ViewModel
                         dodatak = double.Parse(r.GetString("dodatak_na_platu"), System.Globalization.CultureInfo.InvariantCulture);
                         zaposlenjeDatum = DateTime.Parse(datumZaposlenja, new CultureInfo("en-CA"));
                         godisnji = Int32.Parse(r.GetString("dani_godisnjeg_odmora"));
-                        
+
                         //Tip Resolve
                         Uposlenik tmp = new Uposlenik(naziv, adresa, telefon, zaposlenjeDatum, spol, plata, dodatak, godisnji);
                         ListaUposlenika.Add(tmp);
@@ -344,7 +349,7 @@ namespace it_shop.ViewModel
             DMLUpitiNaBazu(upit, connectionBaza);
             //ListaUposlenika.Remove(uposlenik);
         }
-       
+
         private void UnosNovogKorisnikaUBazu()
         {
 
@@ -357,15 +362,77 @@ namespace it_shop.ViewModel
             AdresaAzuriraj = OdabraniUposlenik.Adresa;
             BrojTelefonaAzuriraj = OdabraniUposlenik.BrojTelefona;
             SpolAzuriraj = OdabraniUposlenik.Spol;
-            TipUposlenikaAzuriraj = "Manager";
+            TipUposlenikaAzuriraj = "DIREKTOR";
             DatumZaposlenjaAzuriraj = OdabraniUposlenik.DatumZaposlenja.ToShortDateString();
             PlataAzuriraj = OdabraniUposlenik.Plata.ToString();
-            DodatakNaPlata = OdabraniUposlenik.DodatakNaPlatu.ToString();
+            DodatakNaPlatuAzuriraj = OdabraniUposlenik.DodatakNaPlatu.ToString();
             DaniGodisnjegAzuriraj = OdabraniUposlenik.DaniGodisnjegOdmora.ToString();
             UsernameAzuriraj = "Neko";
             PasswordAzuriraj = "Neko";
         }
 
+        private void AzurirajInformacijeKorisnika()
+        {
+            try
+            {
+                string upit = "UPDATE uposlenici SET ime_i_prezime = '" + ImeAzuriraj + " " + PrezimeAzuriraj + "', spol = '" + SpolAzuriraj + "', adresa = '" +
+                                AdresaAzuriraj + "', broj_telefona = '" + BrojTelefonaAzuriraj + "', tip_uposlenika = '" + TipUposlenikaAzuriraj + "', plata = " + PlataAzuriraj + ", dodatak_na_platu = " + DodatakNaPlatuAzuriraj + ", dani_godisnjeg_odmora = " +
+                                DaniGodisnjegAzuriraj + ", username = '" + UsernameAzuriraj + "', password = '" + PasswordAzuriraj + "' WHERE id = 1;";
+                MessageBox.Show(upit);
+
+                MySqlConnection connectionBaza = new MySqlConnection("server=192.168.1.11; user=root; pwd=root; database=it_shop");
+                DMLUpitiNaBazu(upit, connectionBaza);
+                OcistiFormuZaAzuriranjeInformacijaUposlenih();
+                ListaUposlenika.Clear();
+                UcitajUposlenikeIzBaze();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+        }
+       
+        private void OcistiFormuZaAzuriranjeInformacijaUposlenih()
+        {
+            ImeAzuriraj = String.Empty;
+            PrezimeAzuriraj = String.Empty;
+            AdresaAzuriraj = String.Empty;
+            BrojTelefonaAzuriraj = String.Empty;
+            SpolAzuriraj = String.Empty;
+            TipUposlenikaAzuriraj = String.Empty;
+            DatumZaposlenjaAzuriraj = String.Empty;
+            PlataAzuriraj = String.Empty;
+            DodatakNaPlatuAzuriraj = String.Empty;
+            DaniGodisnjegAzuriraj = String.Empty;
+            UsernameAzuriraj = String.Empty;
+            PasswordAzuriraj = String.Empty;
+        }
+        
+        #endregion
+       
+        
+        #endregion
+
+        #region INotify Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
 
     }
 }
